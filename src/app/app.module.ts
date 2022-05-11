58d2428ff39f5ddeb822cf40df9a1f769f1f7e73
sandrine dushimimana <sanrdushimimana@gmail.com>
2022-05-11 20:57:47 +0200

    Simple angular chatbot

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatService } from './services/chat.service';
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [ChatService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+
+export class Message {
+  constructor(public author: string, public content: string) {}
+}
+@Injectable()
+
+export class ChatService {
+  constructor() {}
+
+   conversation = new Subject<Message[]>();
+   messageMap:any = {
+     "Hi" : "Hello",
+     "Who are you" : "My name is Test Sat Bot",
+     "What is your role" : "Just guide for the user",
+     "defaultmsg" : "I can't understand your text."
+   }
+
+   getBotAnswer(msg:string){
+     const userMessage = new Message('User',msg);
+     this.conversation.next([userMessage]);
+     const botMessage = new Message('bot',this.getBotMessage(msg));
+     setTimeout(() => {
+       this.conversation.next([botMessage]);
+     }, 1500);
+   }
+
+   getBotMessage(question: any){
+     let answer = this.messageMap[question];
+     return answer || this.messageMap['defaultmsg']
+   }
+}
